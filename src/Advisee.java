470ce45d2f26b7dbb2e62566f2758552529ee588
smunsen <smunsen@vassar.edu>
2021-10-07 16:18:02 -0400

    Started methods of Advisee and AdviseeController classes

--- src/Advisee.java
+++ src/Advisee.java
@@ -1,2 +1,12 @@
 public class Advisee {
+    String name;
+    int id;
+    int classYear;
+
+    Advisee(String name, int id, int classYear){
+        this.name = name;
+        this.id = id;
+        this.classYear = classYear;
+    }
+
 }
--- src/AdviseeController.java
+++ src/AdviseeController.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class AdviseeController {
+
+    ArrayList<Advisee> advisees = new ArrayList<>();
+
+    public void addAdvisee(String name, int id, int classYear){
+        Advisee advisee = new Advisee(name, id, classYear);
+        advisees.add(advisee);
+    }
+
+    public void editAdvisee(){}
+
 }
