ae549866b9c1a84a1a549d7c160bb3f734cc7e65
jofosu <jofosu@vassar.edu>
2021-10-07 17:13:00 -0400

    User interface

--- src/Gui.java
+++ src/Gui.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 public class Gui {
     public static void main(String[] args) {
         System.out.println("Hello Advisor, what will you like to do today ?");
-
-        System.out.println();
-
+        AdviseeController advisor = new AdviseeController();
         System.out.println("Choose a number from the following: \n " +
-                "1. Add new advisee \n " +
-                "2. Get advisee information \n " +
-                "3. Add courses \n ");
+                "1. Add new advisee \n ");
 
         Scanner advisorInput = new Scanner(System.in);
         int work = advisorInput.nextInt();
@@ -17,19 +13,23 @@ public class Gui {
         int studentId;
         int studentClassYear;
         String studentName;
+
         switch(work){
 
             case 1: System.out.println("Please enter student's full name");
                     studentName = advisorInput.next();
-                    System.out.println("Please enter student's 9 digit id");
-                    studentId = advisorInput.nextInt();
+                    do{
+                        System.out.println("Please enter student's 9 digit id");
+                        studentId = advisorInput.nextInt();
+                    }while(String.valueOf(studentId).length() !=9);
                     System.out.println("Please enter student's class year");
                     studentClassYear = advisorInput.nextInt();
-                    Advisee studentAdvisee = new Advisee(studentName,studentId,studentClassYear);
+                    if(advisor.addAdvisee(studentName,studentId,studentClassYear))
+                        System.out.println(studentName +" has been added to your advisee list");
+                    else System.out.println("Incorrect input");
                     break;
 
-            case 2: System.out.println("Please enter student 9 digit id");
-                    studentId = advisorInput.nextInt();
+
 
         }
 
