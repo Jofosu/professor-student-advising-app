6f57efbb8100acbbc560df8e55285a5f599fb5eb
kingie <jeangonzalez@vassar.edu>
2021-10-07 16:36:10 -0400

    adding skeletons

--- src/CourseController.java
+++ src/CourseController.java
@@ -1,12 +1,21 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CourseController {
-    ArrayList<Course> courseCatalogue = new ArrayList<>();
+    HashMap<String,Course> courseCatalogue = new HashMap<>();
 
     public void addCourse(String id, String time){
         Course course = new Course(id,time);
-        courseCatalogue.add(course);
+        courseCatalogue.put(id,course);
     }
+
+    public void removeCourse(String id){
+        courseCatalogue.remove(id);
+    }
+
+    public void printCourseList(){
+
+    }
+
 
 }
 
