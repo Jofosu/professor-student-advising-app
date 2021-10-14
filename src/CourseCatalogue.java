3e008bcc928b384753bbce625e16b1715c84a859
junjieliu <!Marveliceamn88!>
2021-10-14 16:46:57 -0400

    Fixed the test classes

--- src/CourseCatalogue.java
+++ src/CourseCatalogue.java
@@ -3,11 +3,19 @@ import java.util.HashMap;
 import java.util.Set;
 
 /**
- *
+ * Contains a HashMap collection of courses named courseCatalog, with the methods to add, remove Courses.
+ * ALso a method to return the collection of courses
  */
 public class CourseCatalogue {
     HashMap<String, Course> courseCatalogue = new HashMap<>();
 
+    /**
+     * Adds a course into the courseCatalogue
+     *
+     * @param id : String for the course's ID. For example: CMPU-20352
+     * @param time: String for the time the course takes place. For example: "Monday 1:30-5:30"
+     * @return boolean: true if the course was successfully put into the courseCatalogue
+     */
     public boolean addCourse(String id, String time) {
         Course course = new Course(id, time);
         courseCatalogue.put(id, course);
@@ -15,6 +23,13 @@ public class CourseCatalogue {
         return courseCatalogue.containsKey(id);
     }
 
+    /**
+     * Removes a course from the courseCatalogue
+     *
+     * @param id : String for the course's ID. For example: CMPU-20352
+     * @return boolean: true if the course was successfully removed from course catalogue.
+     * False if the course ID or course was not in the catalogue to begin with.
+     */
     public boolean removeCourse(String id) {
         if (!courseCatalogue.containsKey(id))
             return false;
@@ -22,6 +37,9 @@ public class CourseCatalogue {
         return !(courseCatalogue.containsKey(id));
     }
 
+    /**
+     * @return The collection of courses in the courseCatalogue
+     */
     public Set<String> returnCourseList() {
         return courseCatalogue.keySet();
     }
