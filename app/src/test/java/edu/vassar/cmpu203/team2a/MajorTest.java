c7a9c09ebe924f6d093397a0e323361f684825ce
junjieliu <!Marveliceamn88!>
2021-11-18 16:24:09 -0500

    created updated xml files/display

---
+++ app/src/test/java/edu/vassar/cmpu203/team2a/MajorTest.java
@@ -0,0 +1,28 @@
+package edu.vassar.cmpu203.team2a;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MajorTest {
+
+    @Test
+    void createPool() {
+    }
+
+    @Test
+    void addCourse() {
+    }
+
+    @Test
+    void getMajor() {
+    }
+
+    @Test
+    void setRequiredPoolCourses() {
+    }
+
+    @Test
+    void removeCourse() {
+    }
+}
