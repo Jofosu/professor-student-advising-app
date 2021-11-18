61cbaab42b8fd5e46e958772b424b504d3c83ee9
smunsen <smunsen@vassar.edu>
2021-11-17 20:59:12 -0500

    Connecting the interface to the logic/controllerActivity

--- app/src/main/java/edu/vassar/cmpu203/team2a/controller/ControllerActivity.java
+++ app/src/main/java/edu/vassar/cmpu203/team2a/controller/ControllerActivity.java
@@ -6,13 +6,14 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.vassar.cmpu203.team2a.view.IMainView;
+import edu.vassar.cmpu203.team2a.view.IManageAdviseesMenu;
 import edu.vassar.cmpu203.team2a.view.MainView;
 import edu.vassar.cmpu203.team2a.view.IOptionsMenu;
 import edu.vassar.cmpu203.team2a.view.OptionsMenu;
 import edu.vassar.cmpu203.team2a.model.*;
 
 
-public class ControllerActivity extends AppCompatActivity implements IOptionsMenu {
+public class ControllerActivity extends AppCompatActivity implements IOptionsMenu.Listener, IManageAdviseesMenu.Listener {
 
     private IMainView mainView;
 
@@ -25,9 +26,30 @@ public class ControllerActivity extends AppCompatActivity implements IOptionsMen
         this.setContentView(this.mainView.getRootView());
        //  this.mainView.displayFragment(new OptionsMenu(this));
     }
-     //this method generates a different screen on a button push
-     //public void onManageAdvisees(){this.mainView.displayFragment(<ManageAdviseesMenuFragment()>)}
+    //TODO switch functionality from Controller class to ControllerActivity class
+    /**
+     *
+     //IOptionsMenu implementation
+     @Override
+     public void onAddCourse(){
+        this.mainView.displayFragment();
+     }
+     @Override
+     public void onMajorReq(){
+     this.mainView.displayFragment();
+     }
+     @Override
+     public void onManageAdvisees(){
+        this.mainView.displayFragment(<ManageAdviseesMenu()>);
+     }
 
+    //IManageAdviseesMenu.Listener implementation
+     @Override
+     void onAddAdvisee();
+     void onDeleteAdvisee();
+     void onEditAdvisee();
+
+*/
 
 
 
--- app/src/main/java/edu/vassar/cmpu203/team2a/view/IManageAdviseesMenuFragment.java
+++
@@ -1,9 +0,0 @@
-package edu.vassar.cmpu203.team2a.view;
-
-public interface IManageAdviseesMenuFragment {
-
-    public interface Listener{
-
-    }
-
-}
