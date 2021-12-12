4abd25820c2a62befe90635f4f83be9c5c662d42
smunsen <smunsen@vassar.edu>
2021-12-12 15:36:29 -0500

    Javadoc

--- app/src/main/java/edu/vassar/cmpu203/team2a/model/AuthKey.java
+++ app/src/main/java/edu/vassar/cmpu203/team2a/model/AuthKey.java
@@ -18,30 +18,61 @@ public class AuthKey implements Serializable {
     private String salt;
     private String key;
 
+    /**
+     * Empty AuthKey Constructor
+     */
     public AuthKey(){ }
 
+    /**
+     * AuthKey Constructor, calls another constructor after generating salt necessary for
+     * saving the password
+     * @param password
+     */
     public AuthKey(String password){
         this(generateSalt(), password);
     }
 
+    /**
+     * AuthKey Constructor that creates the final product with salt and key
+     * @param salt :salt
+     * @param password : password
+     */
     private AuthKey(String salt, String password){
         this.salt = salt;
         this.key = generateKey(salt, password);
     }
 
+    /**
+     * Getter for salt
+     * @return String salt
+     */
     public String getSalt() {
         return this.salt;
     }
 
+    /**
+     * Getter for key
+     * @return String key
+     */
     public String getKey() {
         return this.key;
     }
 
+    /**
+     * Creates a new AuthKey from given password then checks to see if that new key
+     * matches the object's key (this.key)
+     * @param password : password
+     * @return true if the given password matches the one saved
+     */
     public boolean validatePassword(String password){
         AuthKey okey = new AuthKey(this.salt, password);
         return this.key.equals(okey.key);
     }
 
+    /**
+     * toString method for authKey that returns its fields salt and key
+     * @return a String in the format salt: xx, key: xx
+     */
     @Override
     @NonNull
     public String toString() {
@@ -52,6 +83,10 @@ public class AuthKey implements Serializable {
     private static final int KEY_LEN = 40;
     private static final int NITERS = 64000;
 
+    /**
+     * Randomly generates salt for a password
+     * @return a random String
+     */
     @NonNull
     private static String generateSalt() {
         byte[] salt = new byte[SALT_LEN];
@@ -64,6 +99,12 @@ public class AuthKey implements Serializable {
         return Base64.getEncoder().encodeToString(salt);
     }
 
+    /**
+     * Generates the unique key for the given password and salt
+     * @param salt
+     * @param password
+     * @return a string
+     */
     private static String generateKey(String salt, String password) {
         String hashStr = null;
         try {
--- app/src/main/java/edu/vassar/cmpu203/team2a/model/User.java
+++ app/src/main/java/edu/vassar/cmpu203/team2a/model/User.java
@@ -12,23 +12,48 @@ public class User implements Serializable {
     private String username;
     private AuthKey authKey;
 
+    /**
+     * Empty User Constructor
+     */
     public User() {}
 
+    /**
+     * User Constructor
+     * @param username :username
+     * @param password :password
+     */
     public User(String username, String password){
 
         this.username = username;
         this.authKey = new AuthKey(password);
     }
 
+    /**
+     * Getter for username field
+     * @return username
+     */
     public String getUsername(){ return this.username; }
+
+    /**
+     * Getter for AuthKey field
+     * @return authorization key
+     */
     public AuthKey getAuthKey(){ return this.authKey; }
 
+    /**
+     * Checks if password entered matches with the User
+     * @param password
+     * @return true if given password matches with the User
+     */
     public boolean validatePassword(String password){
         return this.authKey.validatePassword(password);
     }
 
     @Override
     @NonNull
+    /**
+     * toString for User, returns a string of the format User xx, authKey: xx
+     */
     public String toString(){
         return String.format("User %s, authKey: %s", this.username, this.authKey.toString());
     }
