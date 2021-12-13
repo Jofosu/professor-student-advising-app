5710560fa64997a13d3872af3b392468fe82ccf6
junjieliu <!Marveliceamn88!>
2021-12-12 21:34:50 -0500

    Updated all the tests to work with the new commits

--- app/src/androidTest/java/edu/vassar/cmpu203/team2a/AddAndRemoveAdviseeTest.java
+++ app/src/androidTest/java/edu/vassar/cmpu203/team2a/AddAndRemoveAdviseeTest.java
@@ -27,38 +27,61 @@ public class AddAndRemoveAdviseeTest {
      * First, it goes to the AdivsorMenu fragment, then adds an advisee and checks to see if they're
      * added to the containerOfAdvisees shown on AdvisorMenu frag. Then it removes them and checks
      * to see that they are no longer in the containerOfAdvisees.
+     *
+     * Test relies on there being a course in the courseCatalogue. The "adviseeMajor" field
+     * should match the prefix of one of these courses.
      */
 
     @Test
     public void testAddAndRemoveAdvisees() {
+        //Need to sign in first
+        Espresso.onView(ViewMatchers.withId(R.id.editTextUsername)).
+                perform(ViewActions.typeText("Junjie"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.editTextPassword)).
+                perform(ViewActions.typeText("abcd"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.registerButton)).perform(click());
+        // double-checking user is registered in case it's deleted from firebase
+        // Also tests the register button first
+        Espresso.onView(ViewMatchers.withId(R.id.signInButton)).perform(click());
+
+
         //adding an advisee
         Espresso.onView(ViewMatchers.withId(R.id.advisorButtton)).perform(click());
         Espresso.onView(ViewMatchers.withId(R.id.addAdviseeButton4)).perform(click());
         Espresso.onView(ViewMatchers.withId(R.id.editFirstName))
-                .perform(ViewActions.typeText("Jun"));
+                .perform(ViewActions.typeText("John"));
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.editMiddleName))
-                .perform(ViewActions.typeText("Jie"));
+                .perform(ViewActions.typeText("Jane"));
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.editLastName))
-                .perform(ViewActions.typeText("Liu"));
+                .perform(ViewActions.typeText("Doe"));
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.editClassYear))
                 .perform(ViewActions.typeText("2022"));
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.editStudentId))
-                .perform(ViewActions.typeText("999516821"));
+                .perform(ViewActions.typeText("999222111"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.adviseeMajor))
+                .perform(ViewActions.typeText("BIOL")); //the prefix to a course
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.endButton)).perform(click()); //create an advisee
 
-        ViewInteraction listofPools = Espresso.onView(ViewMatchers.withId(R.id.listofCourses));
+        /* The list contains button options. Even though the advisee shows up as a new item, I'm
+        unsure how to check it.
+        ViewInteraction listofPools = Espresso.onView(ViewMatchers.withId(R.id.containerOfAdvisees));
         listofPools.check(matches(ViewMatchers.withSubstring("Jun Jie Liu")));
+        */
+
         // checks addition is successful
 
         //removing an advisee
         Espresso.onView(ViewMatchers.withId(R.id.deleteAdviseeButton2)).perform(click());
         Espresso.onView(ViewMatchers.withId(R.id.editStudentId2))
-                .perform(ViewActions.typeText("999516821"));// removes the course
+                .perform(ViewActions.typeText("999222111"));// removes the course
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.button)).perform(click());
         Espresso.onView(ViewMatchers.withSubstring("Jun Jie Liu")).check(doesNotExist());
